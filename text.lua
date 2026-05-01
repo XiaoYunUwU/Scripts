8bdfb12cfafcc5e2ff51a4d7238b35ae82145628
XiaoYunUwU <id_09128616@foxmail.com>
2026-05-01 20:36:44 +0800

    Update text.lua

--- text.lua
+++ text.lua
@@ -1,6 +1,6 @@
 notify({
     Title = "XA：提示",
-    Description = "欢迎加入XA Hub主群：\n996419273",
+    Description = "欢迎加入XA Hub二群：\n996419273",
     Accept = {
         Text = "复制群号",
         Callback = function()
