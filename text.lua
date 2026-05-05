a8ecb790d08c16dff1d827cc6fc6726247d58cba
XiaoYunUwU <id_09128616@foxmail.com>
2026-05-05 11:45:41 +0800

    Update text.lua

--- text.lua
+++ text.lua
@@ -1,16 +1,15 @@
---[[notify({
+notify({
     Title = "XA：提示",
-    Description = "欢迎加入XA Hub主群：\n1057545155",
+    Description = "请加入XA Hub主群：\n1057545155",
     Accept = {
-        Text = "复制群号",
+        Text = "复制群号！",
         Callback = function()
             setclipboard("1057545155")
         end
     },
     Dismiss = {
-        Text = "关闭",
+        Text = "关闭！",
         Callback = function()
         end
     }
 })
-]]
